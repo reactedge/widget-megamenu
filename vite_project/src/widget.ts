ee044ab508781c1046885d504ce535375b21b530
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2026-04-27 18:46:15 +0100

    Increased version

--- vite_project/src/widget.ts
+++ vite_project/src/widget.ts
@@ -1,11 +1,17 @@
-import { mountWidget } from "./mountWidget";
+import {mountWidget, WIDGET_ID} from "./mountWidget";
 import {loadMagentoFonts} from "./services/fontLoader.ts";
 import {loadTranslations} from "./services/translationLoader.ts";
 import type {ResolvedMegamenuConfig} from "./domain/megamenu.types.ts";
 
-export async function mount(el: HTMLElement, config?: ResolvedMegamenuConfig) {
+const mount = async (el: HTMLElement, config?: ResolvedMegamenuConfig) => {
     loadMagentoFonts();
     loadTranslations(el);
 
     await mountWidget(el, config)
 }
+
+const api = { mount };
+
+(window as any)[`ReactEdge_${WIDGET_ID}`] = api;
+
+export { mount };
